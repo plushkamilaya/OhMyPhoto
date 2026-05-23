e2b09ab05f146046b9c5c963d7594ab2405fe8f5
Aleksei Rytikov <user.raa@gmail.com>
2026-05-23 23:15:30 +0200

    Rearrange photo order in Restaurants gallery

--- pages.js
+++ pages.js
@@ -33,9 +33,9 @@ const pages = [
             'ES7A6120.JPG',
             'ES7A4271-3.jpg',
             { src: 'ES7A3590.JPG', wide: 2 },
-            'ES7A6143.JPG',
-            'ES7A6160.JPG',
             'ES7A3612-1.jpg',
+            'ES7A6160.JPG',
+            'ES7A6143.JPG',
             'ES7A3625.JPG',
             'ES7A6156.JPG',
             'ES7A6124.jpg'
